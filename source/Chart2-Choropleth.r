c91ed404b4c5a5cdcbf791fe39a04b3420957906
tjwong22 <tjwong22@uw.edu>
2022-11-14 18:00:49 -0800

    added the choropleth (map of us homelessness by state) chart code

---
+++ source/Chart2-Choropleth.r
@@ -0,0 +1,35 @@
+library(dplyr)
+library(ggplot2)
+library(stringr)
+# directory has to have project inside "info201" folder
+setwd("~/info201/project-group-3-section-ag/data")
+
+# adds and filters numeric data
+homelessness_data2020 <- read.csv("homelessness_2020.csv")
+homeless_summary2020 <- select(homelessness_data2020,
+                               State, Overall.Homeless..2020)
+stringAsFactors = FALSE
+
+# converts string numbers to int ('2,456' -> 2456)
+homeless_summary2020$Overall.Homeless..2020 <- 
+  as.numeric(gsub(",", "", homeless_summary2020$Overall.Homeless..2020))
+
+
+# adds shape file + sets up 'region' for join (with numeric data)
+all_states <- map_data("state")
+all_states[["region"]] <- state.abb[match(str_to_title(all_states[["region"]]),
+                                          state.name)]
+
+# joins the data
+us_homeless_summary2020 <- left_join(all_states, homeless_summary2020,
+                                     by=c("region"="State"))
+
+# creates the graph
+us_homeless_graph <- ggplot(us_homeless_summary2020, aes(long, lat, group=group,
+                                             fill=Overall.Homeless..2020)) +
+  geom_polygon(color="grey") +
+  guides(fill=guide_legend(title="Homelessness Count"))
+
+# print graph
+us_homeless_graph
+
