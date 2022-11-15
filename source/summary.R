11ea9fe1738050d9502dfefe46a376611482dddb
rayy727 <114951420+rayy727@users.noreply.github.com>
2022-11-14 18:18:01 -0800

    Update summary.R
    hi

--- source/summary.R
+++ source/summary.R
@@ -2,8 +2,7 @@
 library(dplyr)
 library(ggplot2)
 library(stringr)
-# directory has to have project inside "info201" folder
+
 setwd("~/info201/project-group-3-section-ag/data")
 
-# adds and filters numeric data
 homelessness_data2020 <- read.csv("homelessness_2020.csv")
