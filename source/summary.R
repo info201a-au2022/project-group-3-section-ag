839cec5a8495015a3680b469b08da8a608cc5d8e
rayy727 <rayy727@uw.edu>
2022-11-14 18:34:45 -0800

    test

--- source/summary.R
+++ source/summary.R
@@ -2,8 +2,9 @@
 library(dplyr)
 library(ggplot2)
 library(stringr)
-# directory has to have project inside "info201" folder
+
 setwd("~/info201/project-group-3-section-ag/data")
 
-# adds and filters numeric data
 homelessness_data2020 <- read.csv("homelessness_2020.csv")
+MSA_Homeless_Population <- read.csv("2018 MSA Homeless Population.csv")
+DHS_Daily_Report <- read.csv("DHS_Daily_Report.csv")
