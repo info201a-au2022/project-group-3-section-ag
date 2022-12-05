1d3028d8f299731ec5e86b92f61f13655b9202be
Brennon <brennonjlee@gmail.com>
2022-12-05 09:56:23 -0800

    Update app_server.R

--- shiny app/app_server.R
+++ shiny app/app_server.R
@@ -4,3 +4,16 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 library(plotly)
+
+#Data frames for plot 1
+homelessness_change <- read.csv('./data/homelessness_change.csv', stringsAsFactors = FALSE)
+homelessness_counts_2020 <- read.csv('./data/homelessness_2020.csv', stringsAsFactors = FALSE)
+
+
+server <- function(input, ouput) {
+  ####### CHART 1
+  output$plot1 <- renderPlotly({
+    plot_1 <- ggplotly(ggplot(
+      data = homelessness_change
+  })
+}
