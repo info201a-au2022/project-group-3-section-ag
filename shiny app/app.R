5a2257669f4742fbf6784f7d2232147992db496c
Brennon <brennonjlee@gmail.com>
2022-12-05 08:57:56 -0800

    Create app.R

---
+++ shiny app/app.R
@@ -0,0 +1,15 @@
+#Library
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(plotly)
+library(shinythemes)
+
+#Sources
+source("shiny_server.R")
+source("shiny_ui.R")
+
+
+#COnnection
+shinyApp(server = shinyServer, ui = shinyUI)
