56478a59c434433099d3d9cc2b4894151c45f006
brennon <brennonjlee@gmail.com>
2022-12-07 09:23:41 -0800

    update

--- shiny_app_2/app_ui.R
+++ shiny_app_2/app_ui.R
@@ -37,7 +37,11 @@ chart_1_panel <- tabPanel(
     
     sidebarPanel(
       chart_1_input,
-      print("")),
+      print("Homelessness varies between each state. Some state's homelessness has been increasing over time while others
+            have been decreasing. Utilizing this chart will allow us to look directly into each state at to see if their
+            homelessness has been increasing or decreasing. By utilizing this data, we can then dive deeper into why that 
+            said state's homelessness was increasing or decreasing. This will hopefully allow us to draw some patterns that 
+            can implement in order for all states to have a decreasing homelessness count and rate.")),
     
     mainPanel(
       plotlyOutput("chart1")
@@ -59,7 +63,7 @@ chart_2_main_content <- mainPanel(
 # display the input slider on the sidebar panel
 chart_2_sidebar_content <- sidebarPanel(
   chart_2_input,
-  print("")
+  print("This second chart provides a visual with color that ")
 )
 
 # creates the page for "Nationwide Homelessness" tab
@@ -77,11 +81,64 @@ chart_2_panel <- tabPanel(
 
 
 ### Summary Tab ###
+summary_main_content <- mainPanel(
+  print("Throughout the project, our group was able to explore the U.S. homelessness data and discover trends that spanned 
+        across the nation and across years. The valuable findings we made were incorporated into various graphs that helped 
+        display the data more clearly as opposed to a table of figures and locations. Among our findings, there were 3 main 
+        takeaways we parted with by the end of the project."),
+  print(" "),
+  h3("Trends of Homelessness Populations Across the Nation (Choropleth Map)"),
+  plotlyOutput("Chart2-Choropleth.r"),
+  print(" "),
+  print("The first research question we sought out was how does homelessness vary across states in the U.S. -- and we were 
+  able to discover this answer through a map of the nation. This map of the U.S. highlights the states where homelessness 
+  is more prominent and its clear to see that California is currently the state with the greatest number of people. This 
+  graph is taken a step further in the interactive tab Nationwide Homelessness as it provides a parameter of homelessness 
+  population that eliminates outliers such as California in order to view the data in greater detail among states with 
+  similar homelessness counts.
+        
+  Viewing this map helps us analyze which areas in the nation are in greater need of help and it also provides an idea of 
+  how geography or population density of a state can lead to a greater or lesser homelessness count."),
+  print(" "),
+  h3("Distribution of Status Among Homelessness Shelters (Pie Chart)"),
+  plotlyOutput("Chart3-Pie.r"),
+  print(" "),
+  print("The second research question we sought out was what factors contribute to the volume of homelessness in a city? 
+  Although the question was not explicitly answered, we managed to analyze one of the factors that have relevance to the 
+  volume of homelessness and that is the distribution of status each individual in homelessness shelters hold. It is clear 
+  that over half of those in homeless shelters are individuals in adult families with children, while the rest of the pie 
+  chart consists of single adults or a slimmer percentage of individuals in purely adult families.
+        
+  Understanding this helps push focus on the groups more likely at risk of homelessness and assuming that it is something 
+  that cannot be avoided, it is important to make efforts to support those groups. As these are families with children, 
+  they have various burdens to carry and needs to fulfill, so lending a hand to these families would be in the nations
+  best interests."),
+  print(" "),
+  h3("Trend of Homelessness Across the Years (Line Chart)"),
+  plotlyOutput("Chart1_Homelessness_Trend.r"),
+  print(" "),
+  print("The last research question we sought was how has the homelessness issue changed over the years? We solved this 
+  question by analyzing the total number of homeless across years 2010-2020. With a 10 year window, we hoped to find a 
+  fitting explanation to the question and we were not disappointed with the result. This graph is refined further to 
+  analyze each state individually in the Homelessness Trends tab. 
+        
+  At the start of the 10 years, homelessness was decreasing at a steady rate until halting around 2016 and remaining 
+  fairly constant over the next 2 years. However, following 2018 homelessness has been rising just as consistantly as it 
+  fell in prior years. With this information, we can compare the numbers to events or policies that may have occurred or 
+  been enforced in those years which either led to an increase or decrease to the homeless population."),
+  print(" "),
+  h3("Closing Thoughts"),
+  print("In conclusion, our analysis was able to answer each of our questions in a manner that helped convey easier to 
+  understand information to viewers. By creating visualizations, we were able to get a better idea of the data we were 
+  tackling, then we refined those visualizations further by developing interactive versions of it. Overall, we were able 
+  to develop our proficiency in R and Shiny while also educating ourselves with the current status of the U.S. homelessness 
+  issue that remains ongoing.")
+)
+
 summary_panel <- tabPanel(
   "Summary Takeaways",
-  mainPanel(
-    includeMarkdown('summary_takeaways.Rmd')
-  )
+  titlePanel("Takeaways From Analysis"),
+  summary_main_content
 )
 
 ### Report Tab ###
@@ -94,7 +151,7 @@ report_panel <- tabPanel(
 )
 
 ### UI Page ###
-ui <- navbarPage(
+shinyUI <- navbarPage(
   "National homelessness",
   intro_panel,
   chart_1_panel,
--- shiny_app_2/intro.md
+++ shiny_app_2/intro.md
@@ -1,26 +1,32 @@
-# Introduction
-
 #### Group 3 Section AG, By: Brennon Lee, Trevor Wong, and Rayna Ojas
     
-![Source: https://www.seattletimes.com/seattle-news/homeless/the-tension-over-visible-homelessness-since-in-the-pandemic-has-risen-in-seattle-and-the-country/](intro_image.png)
-    
+![Source: https://www.seattletimes.com/seattle-news/homeless/the-tension-over-visible-homelessness-since-in-the-pandemic-has-risen-in-seattle-and-the-country/](./www/intro_image.png){#id .class width=50% height=50%}
+
+### Problem
+
 Shelter and housing is something that may seem basic. You probably are fortunate enough to 
 have a roof over your head and a bed to go to at the end of the day. However, across America, 
 there are over half a million people who aren't as fortunate and aren't able to have a 
 roof over their head or a bed to sleep in. This causes many problems and challenges not only for 
-those that are homeless, but also those around them. While you may think that someone being homeless 
-is their own fault or their own choice, more often than not this isn't the case. There are many 
-external reasons why someone may be facing homelessness including the lack of structural supports 
-for those experiencing poverty, job loss that is out of their control, and inadequate help and 
-discharge from people that are leaving hospitals, correctional, or mental health facilities.
-   
+those that are homeless, but also those around them. As shown in the picture above, homelessness
+creates a space in many public areas that are dirty and unsanitary, which imapcts everyone in that
+area. While you may think that someone being homeless is their own fault or their own choice, 
+more often than not this isn't the case. There are many external reasons why someone may be facing 
+homelessness including the lack of structural supports for those experiencing poverty, job loss that
+is out of their control, and inadequate help and discharge from people that are leaving hospitals, 
+correctional, or mental health facilities.
+
+### Our aims
+
 For this project, we aim to investigate the homelessness rates and shelter rates across multiple
 areas in order to determine what factors may be leading to homelessness. We utilize five main datasets
 to pull data from regarding daily homelessness shelter counts, homelessness counts per state in 2020,
 homelessness change in percentage from 2020 to previous years, homelessness counts in major cities in 
 2018, and the homelessness representation ratio for major cities in 2018. Exploring this data is crucial
 to determine patterns between homelessness and other factors that may be causing homelessness.
- 
+
+### Our Questions
+
 The research questions we will be exploring are: How does homelessness vary between states in America and why? 
 What factors may contribute to the volume of homelessness in a city? and In Seattle, how has the homelessness issue 
 changed over the years? Have services offered over the years led to a change in the numbers? We address these 
